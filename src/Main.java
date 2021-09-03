8afb749b152e12ac5087e312fae409092644258d
Mani9732 <mani.shah23@gmail.com>
2021-09-03 18:53:18 -0400

    Refactored Coded

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
 public class Main
 {
 
+	public static void main(String[] args)
+	{
+		PolynomialGenerator polynomialGenerator = new PolynomialGenerator();
+
+		System.out.println("Univariate");
+		polynomialGenerator.univariateMode();
+		String poly1 = polynomialGenerator.generatePolynomial();
+		System.out.println(poly1);
+
+		System.out.println("Bivariate");
+		polynomialGenerator.bivariateMode();
+		String poly2 = polynomialGenerator.generatePolynomial();
+		System.out.println(poly2);
+
+		System.out.println("Trivariate");
+		polynomialGenerator.trivariateMode();
+		String poly3 = polynomialGenerator.generatePolynomial();
+		System.out.println(poly3);
+
+	}
 }
